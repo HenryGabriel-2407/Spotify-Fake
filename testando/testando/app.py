261ae92fcce8f9a70e7bdaa7122989d050beb863
Henry Gabriel <63942305+HenryGabriel-2407@users.noreply.github.com>
2024-09-19 23:47:44 -0300

    :de:

--- testando/testando/app.py
+++ testando/testando/app.py
@@ -2,18 +2,17 @@ from fastapi import FastAPI, Query, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from testando.schemas import Musica, Musicas
-
 import base64
 import requests
 import random
+import time
 
-
-# Inicializando o fastapi
+# Inicializando o FastAPI
 app = FastAPI()
 
 # Informações de autenticação do Spotify
-client_id = "03d8c173d4dc4b2fadfc95c767e82645"
-client_secret = "1fc63f29ccfd4a0e8d5769073a137964"
+client_id = "286534037b3d4e109e05ffb401e34262"
+client_secret = "0a68a5e5012747308a08220cd141d136"
 
 # URL para obter o token de acesso
 token_url = "https://accounts.spotify.com/api/token"
@@ -31,6 +30,9 @@ headers = {
     'Content-Type': 'application/x-www-form-urlencoded'
 }
 
+# Variáveis globais para armazenar o token e o tempo de expiração
+access_token = None
+token_expires_at = 0
 
 # Configurar a pasta estática
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -45,72 +47,116 @@ def serve_html():
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Erro ao carregar a página: {e}")
 
-# Função para verificar se o user possui autorização de uso
 def get_access_token():
-    response = requests.post(token_url, headers=headers, data=data)
-    if response.status_code == 200:
-        token_info = response.json()
-        return token_info['access_token']
-    else:
-        return None
+    global access_token, token_expires_at
 
+    # Se o token já estiver definido e ainda não expirou, reutilize-o
+    if access_token and time.time() < token_expires_at:
+        return access_token
+
+    # Caso contrário, faça uma nova solicitação para obter um novo token
+    try:
+        response = requests.post(token_url, headers=headers, data=data, timeout=10)
+        response.raise_for_status()
+        token_info = response.json()
+        access_token = token_info['access_token']
+        token_expires_at = time.time() + token_info['expires_in']
+        return access_token
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao obter token do Spotify: {e}")
+
+def get_with_retry(url, headers, params=None, max_retries=5):
+    retries = 0
+    while retries < max_retries:
+        response = requests.get(url, headers=headers, params=params)
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 429:  # Too Many Requests
+            wait_time = 2 ** retries  # Exponential backoff
+            print(f"Rate limit exceeded. Retrying in {wait_time} seconds...")
+            time.sleep(wait_time)
+            retries += 1
+        else:
+            response.raise_for_status()
+    raise HTTPException(status_code=429, detail="Rate limit exceeded after multiple retries.")
+
+@app.get("/suggestions/")
+def get_suggestions(q: str = Query(...)):
+    try:
+        access_token = get_access_token()
+        search_headers = {
+            'Authorization': f'Bearer {access_token}'
+        }
+
+        search_params = {
+            'q': q,
+            'type': 'track',
+            'limit': 5  # Limitar as sugestões
+        }
+        search_url = "https://api.spotify.com/v1/search"
+        response = requests.get(search_url, headers=search_headers, params=search_params)
+        response.raise_for_status()
+
+        search_results = response.json()
+        tracks = search_results.get('tracks', {}).get('items', [])
+
+        suggestions = [{'name': track['name']} for track in tracks]
+
+        return {"suggestions": suggestions}
+
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao buscar sugestões: {e}")
 
 @app.get("/search/", response_model=Musicas)
 def buscar_musica(q: str = Query(None, description="Nome da música ou artista a ser buscado")):
-    access_token = get_access_token() 
-    if not access_token:
-        return Musicas(musicas=[])
+    try:
+        access_token = get_access_token()
+        if not access_token:
+            return Musicas(musicas=[])
 
-    search_headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
-    
-    # lista de músicas
-    results = []
+        search_headers = {
+            'Authorization': f'Bearer {access_token}'
+        }
 
-    # Buscar por músicas
-    search_params = {
-        'q': f'track:"{q}"',
-        'type': 'track',
-        'limit': 20
-    }
-    search_url = "https://api.spotify.com/v1/search"
-    response = requests.get(search_url, headers=search_headers, params=search_params)
-    
-    # a pesquisa (GET) retornou 200?
-    if response.status_code == 200:
-        search_results = response.json()
+        results = []
+
+        # Buscar por músicas
+        search_params = {
+            'q': f'track:"{q}"',
+            'type': 'track',
+            'limit': 20
+        }
+        search_url = "https://api.spotify.com/v1/search"
+        search_results = get_with_retry(search_url, search_headers, search_params)
+
         tracks = search_results.get('tracks', {}).get('items', [])
         if tracks:
             results.extend([Musica(link=f"https://open.spotify.com/embed/track/{track['id']}") for track in tracks])
 
-    # Buscar por artistas
-    search_params = {
-        'q': q,
-        'type': 'artist',
-        'limit': 5 
-    }
-    response = requests.get(search_url, headers=search_headers, params=search_params)
-    
-    if response.status_code == 200:
-        search_results = response.json()
-        artists = search_results.get('artists', {}).get('items', [])
-        
+        # Buscar por artistas
+        search_params = {
+            'q': q,
+            'type': 'artist',
+            'limit': 5
+        }
+        artist_results = get_with_retry(search_url, search_headers, search_params)
+
+        artists = artist_results.get('artists', {}).get('items', [])
         if artists:
             artist_id = artists[0]['id']
-            # Buscar as músicas do artista
             tracks_params = {
-                'limit': 20
+                'limit': 20,
+                'market': 'US'  # Mercado que deseja buscar (US, BR, etc.)
             }
             tracks_url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks"
-            response = requests.get(tracks_url, headers=search_headers, params=tracks_params)
-            
-            if response.status_code == 200:
-                tracks_results = response.json()
-                tracks = tracks_results.get('tracks', [])
-                
-                results.extend([Musica(link=f"https://open.spotify.com/embed/track/{track['id']}") for track in tracks])
+            tracks_results = get_with_retry(tracks_url, search_headers, tracks_params)
 
-    # Embaralhar a lista de resultados antes de retornar
-    random.shuffle(results)
-    return Musicas(musicas=results)
+            tracks = tracks_results.get('tracks', [])
+            results.extend([Musica(link=f"https://open.spotify.com/embed/track/{track['id']}") for track in tracks])
+
+        # Embaralhar a lista de resultados antes de retornar
+        random.shuffle(results)
+        return Musicas(musicas=results)
+
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=f"Erro ao buscar músicas/artistas: {e}")
